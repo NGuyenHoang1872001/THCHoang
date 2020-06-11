39a6464c61a6252a76d69803f84ae5471679e940
hoang <nguyenhoang1872011@gmail.com>
2020-06-11 15:12:03 +0700

    First commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int[] number = new int[30];
+            Console.WriteLine("enter the value of number");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the number");
+            int i = int.Parse(Console.ReadLine());
+;            for (i = 0; i < n; i++) ;
+            {
+                int j;
+                for (j = i + 1; j < n; j++) ;
+                {
+                    if (number[i] > number[j]) ;
+                }
+                int a = number[i];
+                number[i] = number[j];
+                number[j] = a;
+            }
+            for (i=0;i<n;i++)
+            {
+                Console.WriteLine("the number arranged in ascending order are given below");
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("how many number?");
+            int n = int.Parse(Console.ReadLine())
+            int[] number = new int[10];
+            for (int i = 0; i < n; i++) 
+            {
+                Console.WriteLine("enter number {0}", i);
+                int value = int.Parse(Console.ReadLine());
+                number[i]=value;
+            }
+            int max = number[0];
+            int min = number[0];
+            for (int i = 1; i < n; i++) 
+            {
+                if (number[i] > max) 
+                {
+                    max = number[i];
+                }
+                if (number[i]<min)
+                {
+                    min = number[i];
+                }
+            }
+            Console.WriteLine("the highest number in the array is {0}", max);
+            Console.WriteLine("the lowest numbe in the array is {0}", min);
+        }
+    }
+}
+
